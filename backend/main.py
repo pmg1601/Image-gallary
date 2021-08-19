d39b3b9142129fb2e672f1f5a4d9b4612ec1342b
Pratik <pratikghodke4@gmail.com>
2021-08-19 17:51:38 +0530

    Created Python API

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Unsplash Key cannot be NULL!")
+
+app = Flask(__name__)
+
+# New Image GET API call
+@app.route("/new-image")
+def newImage():
+    word = request.args.get("query")
+    headers = {"Accept-version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
+
+    respose = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = respose.json()
+
+    return data
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
